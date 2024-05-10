9710648a26542ad885d7c982757952b8658c51ee
Bieelogrom <gabrielamorim15436@gmail.com>
2024-05-10 20:20:11 -0300

    Test 2 - CI

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <p>Gabriel</p>
+    <p>Principal</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
