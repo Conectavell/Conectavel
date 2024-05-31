b3228ebb1359842946eeb4892480542ace58ac8f
Ana Rodrigues <rodriguesanajulia000@gmail.com>
2024-05-31 03:02:45 -0300

    header e comoFunciona ajustados

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
 import styled from 'styled-components'
-import { Detail } from './Header'
 
 const Box = styled.div`
     width: 100%;
     display: flex;
     flex-direction: row;
-    border: 1px solid #5EC460;
+    border: 1px solid var(--verde_principal);
     border-radius: 12px;
     margin-bottom: 2rem;
-
-    
     .step{
-        border: 1px solid #5EC460;
+        border: 1px solid var(--verde_principal);
         border-radius: 90px;
-        font-size: 12px;
+        font-size: 18px;
         padding: .1rem .5rem;
     }
     .title{
         font-family: 'Poppins';
         font-style: normal;
         font-weight: 400;
-        font-size: 24px;
+        font-size: 26px;
         line-height: 140%;
     }
 
@@ -41,8 +37,13 @@ const Content = styled.div`
     gap: .5rem;
 
     @media (max-width: 768px){
-       width: 100%;
+        width: 100%;
     }
+`
+const Texto = styled.p`
+    text-align: center;
+    color: #95979C;
+    font-size: 20px;
 `
 
 const Step = ({ titulo1, titulo2, titulo3, text1, text2, text3 }) => {
@@ -53,17 +54,17 @@ const Step = ({ titulo1, titulo2, titulo3, text1, text2, text3 }) => {
             <Content>
                 <p className='step'>Step 1</p>
                 <h4 className='title'>{titulo1}</h4>
-                <Detail style={{ textAlign: 'center' }}>{text1}</Detail>
+                <Texto>{text1}</Texto>
             </Content>
-            <Content style={width > 768 ? { borderLeft: '1px solid #5EC460' } : { borderTop: '1px solid #5EC460' }}>
+            <Content style={width > 768 ? { borderLeft: '1px solid var(--verde_principal)' } : { borderTop: '1px solid var(--verde_principal)' }}>
                 <p className='step'>Step 2</p>
                 <h4 className='title'>{titulo2}</h4>
-                <Detail style={{ textAlign: 'center' }}>{text2}</Detail>
+                <Texto>{text2}</Texto>
             </Content>
-            <Content style={width > 768 ? { borderLeft: '1px solid #5EC460' } : { borderTop: '1px solid #5EC460' }}>
+            <Content style={width > 768 ? { borderLeft: '1px solid var(--verde_principal)' } : { borderTop: '1px solid var(--verde_principal)' }}>
                 <p className='step'>Step 3</p>
                 <h4 className='title'>{titulo3}</h4>
-                <Detail style={{ textAlign: 'center' }}>{text3}</Detail>
+                <Texto>{text3}</Texto>
             </Content>
         </Box>
     )
