c25101a73adcb24f39e9ce6f4128284964d27aa9
Bieelogrom <gabrielamorim15436@gmail.com>
2024-06-09 12:18:20 -0300

    Inclusão Chat - 1.3

--- src/pages/CompletarInfoPrestadorPage.jsx
+++ src/pages/CompletarInfoPrestadorPage.jsx
@@ -1,38 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-import Navbar from '../components/NavbarPerfis'
+import Navbar from '../components/NavbarPerfis';
+import ChatMini from '../components/ChatMini';
+import Kemilly from '../assets/kemilly.png'
 
 const SectionWrapper = styled.section`
   width: 100%;
   height: 100%;
+  padding: 12px;
 `
 
 const ChatLateral = styled.div`
   width: 313px;
   height: 100%;
+  padding: 12px;
   border: 2px #EBEBEE solid;
   border-radius: 10px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.15);
-
 `
 
-const MainWrapper = styled.main`
+const ChatLateralH1 = styled.h1`
+  font-size: 24px;
+  color: #222222;
+`
+
+const ChatLateralChats = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  padding-top: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 `
-
-
 
 const CompletarInfoPrestadorPage = () => {
   return (
     <>
+      <Navbar />
       <SectionWrapper>
-        <Navbar />
-        <MainWrapper>
-          <ChatLateral>
-
-          </ChatLateral>
-        </MainWrapper> 
+        <ChatLateral>
+          <ChatLateralH1>Chat</ChatLateralH1>
+          <ChatLateralChats>
+            <ChatMini foto={Kemilly} nome={"Sarah Doe"} descricao={"Muito obrigada pelo reparo!"}/>
+            <ChatMini foto={Kemilly} nome={"Sarah Doe"} descricao={"Muito obrigada pelo reparo!"}/>
+            <ChatMini foto={Kemilly} nome={"Sarah Doe"} descricao={"Muito obrigada pelo reparo!"}/>
+            <ChatMini foto={Kemilly} nome={"Sarah Doe"} descricao={"Muito obrigada pelo reparo!"}/>
+          </ChatLateralChats>
+        </ChatLateral>
       </SectionWrapper>
     </>
   )
