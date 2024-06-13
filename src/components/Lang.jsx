6b69d33bcbcb3a18e978a395d59c2b8b05e70b95
Ana Rodrigues <rodriguesanajulia000@gmail.com>
2024-06-13 00:02:07 -0300

    tradução

--- src/components/Lang.jsx
+++ src/components/Lang.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import br from '../assets/br.png'
+import en from '../assets/en.png'
+import jp from '../assets/jp.jpg'
+import { useTranslation } from "react-i18next";
+
 
 const LangBox = styled.a`
     text-decoration: none;
@@ -11,10 +17,39 @@ const LangBox = styled.a`
     }
 `
 
-const Lang = ({src}) => {
+const Lang = () => {
+    const { i18n } = useTranslation();
+
+
+
+    const [src, setSrc] = useState(br)
+    const [textoAlternativo, setTextoAlternativo] = useState(["pt","ja","en"])
+    const [linguagem,setLinguagem] = useState([br, en, jp])
+    const [count, setCount] = useState(0)
+
+    const handleChangeLanguage = (value) => {
+        i18n.changeLanguage(value);
+    };
+
+    const mudarLingua = () => {
+        setCount(prevCount => {
+            const newCount = (prevCount + 1) % linguagem.length;
+            setSrc(linguagem[newCount]);
+            handleChangeLanguage(textoAlternativo[newCount]);
+            return newCount;
+        });
+    };
+      
+
+
     return (
         <LangBox>
-            <img src={src} alt='' />
+            <img
+                onClick={
+                    () => {
+                        mudarLingua()
+                    }}
+                src={src} alt={`Bandeira ${textoAlternativo[count]}`} />
         </LangBox>
     )
 }
