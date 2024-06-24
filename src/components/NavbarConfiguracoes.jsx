7a6932b477fff8fb67d6baed47fb33efefc5fc55
Ana Rodrigues <rodriguesanajulia000@gmail.com>
2024-06-24 11:55:17 -0300

    barra de navegação perfis e configuracoes com rota

--- src/components/NavbarConfiguracoes.jsx
+++ src/components/NavbarConfiguracoes.jsx
@@ -6,10 +6,13 @@ import { FaBarsStaggered } from 'react-icons/fa6';
 import { RiHome2Line } from 'react-icons/ri';
 import { FaUserCircle } from 'react-icons/fa';
 import { IoLockClosed } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import CadastroContext from '../context/CadastroContext';
 
 
 const NavbarConfiguracoes = () => {
-
+    const { tipoPerfil } = useContext(CadastroContext)
     return (
         <Navbar expand="lg" className="navbar-perfil p-3">
             <Container className='navbar__container'>
@@ -19,9 +22,9 @@ const NavbarConfiguracoes = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse className='text-center' id="basic-navbar-nav">
                     <Nav className="m-auto">
-                        <Nav.Link className='navbar__container__collapse__text' ><RiHome2Line  size={25} /> </Nav.Link>
-                        <Nav.Link className='navbar__container__collapse__text' ><FaUserCircle size={25} /> Gerenciar configurações</Nav.Link>
-                        <Nav.Link className='navbar__container__collapse__text' ><IoLockClosed  size={25} /> Segurança</Nav.Link>
+                        <Nav.Link  ><Link className='navbar__container__collapse__text' to={`/Conectavel/${tipoPerfil === 1 ? "perfilcliente" : "perfilprestador"}`}><RiHome2Line size={25} /> </Link></Nav.Link>
+                        <Nav.Link  ><Link className='navbar__container__collapse__text' to="/Conectavel/configuracoes"><FaUserCircle size={25} /> Gerenciar configurações</Link></Nav.Link>
+                        <Nav.Link  ><Link className='navbar__container__collapse__text' to="/Conectavel/configuracoes/seguranca"><IoLockClosed size={25} /> Segurança</Link></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
 
--- src/components/NavbarPerfis.jsx
+++ src/components/NavbarPerfis.jsx
@@ -13,6 +13,7 @@ import { PiUsersThree } from 'react-icons/pi';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import CadastroContext from '../context/CadastroContext';
 import { RiHome2Line } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const NavbarPerfis = () => {
     const { innerWidth: width } = window
@@ -23,29 +24,31 @@ const NavbarPerfis = () => {
         <Navbar expand="lg" className="navbar-perfil background">
             <Container className='navbar__container'>
                 <Navbar.Brand href="#home">
+                    <Link to="/Conectavel">
                     <img width={70} src={logo} alt="" />
                     <img width={129} src={logotipo} alt="" />
+                    </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <FaBarsStaggered color='white' />
                 </Navbar.Toggle>
                 <Navbar.Collapse className='navbar__container__collapse' id="basic-navbar-nav">
                     <Nav className="me-auto navbar__container__collapse__content">
-                        <Nav.Link className='navbar__container__collapse__text' ><RiHome2Line  size={25} /></Nav.Link>
+                        <Nav.Link ><Link className='navbar__container__collapse__text' to={`/Conectavel/${tipoPerfil === 1 ? "perfilcliente" : "perfilprestador"}`}><RiHome2Line  size={25} /></Link></Nav.Link>
                         {
                             width > 992 ?
                                 ''
                                 :
-                                <Nav.Link className='navbar__container__collapse__text' ><BemVindo  /></Nav.Link>
+                                <Nav.Link> <Link className='navbar__container__collapse__text'><BemVindo  /></Link></Nav.Link>
                         }
                         {tipoPerfil === 2 ?
                             <>
-                                <Nav.Link className='navbar__container__collapse__text' ><HiOutlineChatBubbleBottomCenterText size={25} /> Chat</Nav.Link>
-                                <Nav.Link className='navbar__container__collapse__text' ><PiUsersThree size={25} /> Comunidade</Nav.Link>
+                                <Nav.Link> <Link className='navbar__container__collapse__text' to="/Conectavel/chatprofissional"><HiOutlineChatBubbleBottomCenterText size={25} /> Chat</Link></Nav.Link>
+                                <Nav.Link> <Link className='navbar__container__collapse__text' to="/Conectavel/comunidade"><PiUsersThree size={25} /> Comunidade</Link></Nav.Link>
                             </> : ""}
 
-                        <Nav.Link className='navbar__container__collapse__text' ><AiOutlineSetting size={25} /> Configurações</Nav.Link>
-                        <Nav.Link className='navbar__container__collapse__text' ><BiRecycle size={25} /> Descarte</Nav.Link>
+                        <Nav.Link><Link className='navbar__container__collapse__text' to="/Conectavel/configuracoes"><AiOutlineSetting size={25}  /> Configurações</Link></Nav.Link>
+                        <Nav.Link> <Link className='navbar__container__collapse__text' to="/Conectavel/descarte"><BiRecycle size={25} /> Descarte</Link></Nav.Link>
 
 
                     </Nav>
